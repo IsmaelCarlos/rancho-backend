b7e07c4ad820a7e3af46ffa847cbeae7909cc0be
ismael <ismael@ismael.com>
2024-05-16 16:55:41 -0300

    cadastro de medicamento funcionando com a correção da data

--- src/routes/medicamento.ts
+++ src/routes/medicamento.ts
@@ -3,6 +3,25 @@ import knex from '../db';
 
 const router = Router();
 
+router.post('/', async (req, res) => {
+    try{
+        /**
+         * No PostGres eu posso passar um segundo parametro
+         * para o insert do knex escolhendo as colunas
+         * da tabela que eu acabei de inserir que serão retornadas
+         */
+        const result = await knex('medicamento').insert(req.body, '*');
+        await (() => new Promise(r => {
+            setTimeout(() => r(true), 5000);
+        }))();
+        res.json({ result });
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({ message: 'Insert falhou' });
+    }
+});
+
 router.get('/', async (req, res) => {
     const resultado =  await knex('medicamento_view').select('*');
     res.json(resultado);
